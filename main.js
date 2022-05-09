ef6605cbc446adf72976eef082b17e8ec05cf568
Bobjoerukes <yo.bob.joe.rules@gmail.com>
2022-05-09 11:25:36 -0700

    added part

--- main.js
+++ main.js
@@ -1,6 +1,5 @@
 const parallax = document.getElementById("parallax");
 const lols = document.getElementById("lols");
-var video = document.getElementById("myVideo");
 window.addEventListener("scroll", function(){
     let offset = window.pageYOffset;
     parallax.style.backgroundPositionY = offset * 0.7 + 'px';
