3363fc86d03e3971cb95e3857ade47a0f8751be3
bobjoerules <yo.bob.joe.rules@gmail.com>
2023-12-01 11:46:53 -0800

    more cool stuff

--- main.js
+++ main.js
@@ -1,9 +1,11 @@
 const parallax = document.getElementById("parallax");
 const lols = document.getElementById("lols");
+const game = document.getElementById("game");
 window.addEventListener("scroll", function(){
     let offset = window.pageYOffset;
     parallax.style.backgroundPositionY = offset * 0.7 + 'px';
     lols.style.backgroundPositionY = offset * 2 + 'px';
+    game.style.backgroundPositionY = offset * 0.5 -40 + 'px';
 })
 const allElements = document.querySelectorAll('*');
 allElements.forEach(element => {
